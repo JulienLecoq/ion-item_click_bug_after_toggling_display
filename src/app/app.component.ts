f0d8f02b865da117ac9482977b38e31cf3667c0d
Julien Lecoq <julien_lecoq@protonmail.com>
2024-08-12 14:53:34 +0200

    Some refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core'
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonCheckbox, IonButton, IonLabel, IonSpinner } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonCheckbox,
+    IonLabel,
+    IonSpinner,
+    IonButton,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  showCheckbox = signal(true)
+
+  stateChange() {
+    console.log('Checkbox state changed.')
+  }
+
+  toggleDisplay() {
+    this.showCheckbox.set(!this.showCheckbox())
+  }
 }
